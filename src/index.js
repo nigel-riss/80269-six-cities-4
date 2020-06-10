535fe78c564bb86c3913678905a06dac0e630262
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-06-10 21:15:41 +0200

    1.3. Add main and app components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-//
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App} from './components/app.jsx';
+
+
+const init = () => {
+  ReactDOM.render(
+      <App
+      />,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
