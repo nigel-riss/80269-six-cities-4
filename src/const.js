28e13f9ee0a35af23639aca1ff998371a79750b5
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-07-02 23:13:53 +0200

    3.1. Add card mocks

---
+++ src/const.js
@@ -0,0 +1,6 @@
+export const PlaceTypes = {
+  APPARTMENT: `apartment`,
+  ROOM: `room`,
+  HOUSE: `house`,
+  HOTEL: `hotel`,
+};
---
+++ src/mocks/offers.js
@@ -0,0 +1,33 @@
+import {PlaceTypes} from '../const';
+
+export default [
+  {
+    photo: `apartment-01.jpg`,
+    isPremium: false,
+    price: 70,
+    description: `Canal View Prinsengracht`,
+    type: PlaceTypes.HOTEL,
+    rating: 3.7,
+  }, {
+    photo: `apartment-02.jpg`,
+    isPremium: false,
+    price: 150,
+    description: `Nice, cozy, warm big bed apartment`,
+    type: PlaceTypes.APPARTMENT,
+    rating: 4.7,
+  }, {
+    photo: `apartment-03.jpg`,
+    isPremium: true,
+    price: 300,
+    description: `Beautiful &amp; luxurious house at great location`,
+    type: PlaceTypes.HOUSE,
+    rating: 4.5,
+  }, {
+    photo: `room.jpg`,
+    isPremium: false,
+    price: 20,
+    description: `Wood and stone place`,
+    type: PlaceTypes.ROOM,
+    rating: 3.3,
+  },
+];
