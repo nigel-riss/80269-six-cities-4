c623d891f43ae4b344df6a127121430852ae31fd
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-07-27 17:14:45 +0200

    5.2. Add useless NoPlaces component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import CityList from '../city-list/city-list.jsx';
 import PlaceList from '../place-list/place-list.jsx';
 import Map from '../map/map.jsx';
+import NoPlaces from '../no-places/no-places.jsx';
 import OfferTypes from '../../types/offer.js';
 import {getCityList} from '../../utils/common.js';
 
@@ -22,6 +23,7 @@ class Main extends PureComponent {
     } = this.props;
 
     const cities = getCityList(offers);
+    cities.push(`Nowhere`);
 
     const {
       latitude,
@@ -64,46 +66,52 @@ class Main extends PureComponent {
             />
           </div>
           <div className="cities">
-            <div className="cities__places-container container">
-              <section className="cities__places places">
-                <h2 className="visually-hidden">Places</h2>
-                <b className="places__found">{activeOffers.length} place{activeOffers.length === 1 ? `` : `s`} to stay in {activeCity}</b>
-                <form className="places__sorting" action="#" method="get">
-                  <span className="places__sorting-caption">Sort by </span>
-                  <span className="places__sorting-type" tabIndex="0">
-                    Popular
-                    <svg className="places__sorting-arrow" width="7" height="4">
-                      <use xlinkHref="#icon-arrow-select"></use>
-                    </svg>
-                  </span>
-                  {/* <ul className="places__options places__options--custom places__options--opened">
-                    <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                    <li className="places__option" tabIndex="0">Price: low to high</li>
-                    <li className="places__option" tabIndex="0">Price: high to low</li>
-                    <li className="places__option" tabIndex="0">Top rated first</li>
-                  </ul>
-                  <select className="places__sorting-type" id="places-sorting">
-                    <option className="places__option" value="popular" selected="">Popular</option>
-                    <option className="places__option" value="to-high">Price: low to high</option>
-                    <option className="places__option" value="to-low">Price: high to low</option>
-                    <option className="places__option" value="top-rated">Top rated first</option>
-                  </select> */}
-                </form>
-                <PlaceList
-                  offers={activeOffers}
-                  onCardTitleClick={onCardTitleClick}
-                />
-              </section>
-              <div className="cities__right-section">
-                <section className="cities__map map">
-                  <Map
-                    center={[latitude, longitude]}
-                    zoom={zoom}
+            {activeOffers.length === 0 ? (
+              <NoPlaces
+                activeCity={activeCity}
+              />
+            ) : (
+              <div className="cities__places-container container">
+                <section className="cities__places places">
+                  <h2 className="visually-hidden">Places</h2>
+                  <b className="places__found">{activeOffers.length} place{activeOffers.length === 1 ? `` : `s`} to stay in {activeCity}</b>
+                  <form className="places__sorting" action="#" method="get">
+                    <span className="places__sorting-caption">Sort by </span>
+                    <span className="places__sorting-type" tabIndex="0">
+                      Popular
+                      <svg className="places__sorting-arrow" width="7" height="4">
+                        <use xlinkHref="#icon-arrow-select"></use>
+                      </svg>
+                    </span>
+                    {/* <ul className="places__options places__options--custom places__options--opened">
+                      <li className="places__option places__option--active" tabIndex="0">Popular</li>
+                      <li className="places__option" tabIndex="0">Price: low to high</li>
+                      <li className="places__option" tabIndex="0">Price: high to low</li>
+                      <li className="places__option" tabIndex="0">Top rated first</li>
+                    </ul>
+                    <select className="places__sorting-type" id="places-sorting">
+                      <option className="places__option" value="popular" selected="">Popular</option>
+                      <option className="places__option" value="to-high">Price: low to high</option>
+                      <option className="places__option" value="to-low">Price: high to low</option>
+                      <option className="places__option" value="top-rated">Top rated first</option>
+                    </select> */}
+                  </form>
+                  <PlaceList
                     offers={activeOffers}
+                    onCardTitleClick={onCardTitleClick}
                   />
                 </section>
+                <div className="cities__right-section">
+                  <section className="cities__map map">
+                    <Map
+                      center={[latitude, longitude]}
+                      zoom={zoom}
+                      offers={activeOffers}
+                    />
+                  </section>
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </main>
       </div>
