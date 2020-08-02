d26cda70f1a40b0ba73e5800524d75bbefc31949
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-08-03 01:50:05 +0200

    6.1. Refactor components to functional and update snapshot tests

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import CityList from '../city-list/city-list.jsx';
 import PlaceSorting from '../place-sorting/place-sorting.jsx';
@@ -13,100 +13,98 @@ import {getCityList} from '../../utils/common.js';
 const PlaceSortingWrapped = withOpenedList(PlaceSorting);
 
 
-class Main extends PureComponent {
-  render() {
-    const {
-      activeCity,
-      activeOffers,
-      activeSort,
-      hoveredItem: hoveredOffer,
-      offers,
-      onCardTitleClick,
-      onCityNameClick,
-      onItemMouseEnter: onPlaceCardMouseEnter,
-      onSortTypeSelect,
-    } = this.props;
+const Main = (props) => {
+  const {
+    activeCity,
+    activeOffers,
+    activeSort,
+    hoveredItem: hoveredOffer,
+    offers,
+    onCardTitleClick,
+    onCityNameClick,
+    onItemMouseEnter: onPlaceCardMouseEnter,
+    onSortTypeSelect,
+  } = props;
 
-    const cities = getCityList(offers);
+  const cities = getCityList(offers);
 
-    const {
-      latitude,
-      longitude,
-      zoom,
-    } = activeOffers[0].city.location;
+  const {
+    latitude,
+    longitude,
+    zoom,
+  } = activeOffers[0].city.location;
 
-    return (
-      <div className="page page--gray page--main">
-        <header className="header">
-          <div className="container">
-            <div className="header__wrapper">
-              <div className="header__left">
-                <a className="header__logo-link header__logo-link--active">
-                  <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-                </a>
-              </div>
-              <nav className="header__nav">
-                <ul className="header__nav-list">
-                  <li className="header__nav-item user">
-                    <a className="header__nav-link header__nav-link--profile" href="#">
-                      <div className="header__avatar-wrapper user__avatar-wrapper">
-                      </div>
-                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
+  return (
+    <div className="page page--gray page--main">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link header__logo-link--active">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+              </a>
             </div>
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <a className="header__nav-link header__nav-link--profile" href="#">
+                    <div className="header__avatar-wrapper user__avatar-wrapper">
+                    </div>
+                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
           </div>
-        </header>
+        </div>
+      </header>
 
-        <main className="page__main page__main--index">
-          <h1 className="visually-hidden">Cities</h1>
-          <div className="tabs">
-            <CityList
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">Cities</h1>
+        <div className="tabs">
+          <CityList
+            activeCity={activeCity}
+            cities={cities}
+            onCityNameClick={onCityNameClick}
+          />
+        </div>
+        <div className="cities">
+          {activeOffers.length === 0 ? (
+            <NoPlaces
               activeCity={activeCity}
-              cities={cities}
-              onCityNameClick={onCityNameClick}
             />
-          </div>
-          <div className="cities">
-            {activeOffers.length === 0 ? (
-              <NoPlaces
-                activeCity={activeCity}
-              />
-            ) : (
-              <div className="cities__places-container container">
-                <section className="cities__places places">
-                  <h2 className="visually-hidden">Places</h2>
-                  <b className="places__found">{activeOffers.length} place{activeOffers.length === 1 ? `` : `s`} to stay in {activeCity}</b>
-                  <PlaceSortingWrapped
-                    activeSort={activeSort}
-                    onSortTypeSelect={onSortTypeSelect}
-                  />
-                  <PlaceList
-                    offers={activeSort.func(activeOffers)}
-                    onCardTitleClick={onCardTitleClick}
-                    onPlaceCardMouseEnter={onPlaceCardMouseEnter}
+          ) : (
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{activeOffers.length} place{activeOffers.length === 1 ? `` : `s`} to stay in {activeCity}</b>
+                <PlaceSortingWrapped
+                  activeSort={activeSort}
+                  onSortTypeSelect={onSortTypeSelect}
+                />
+                <PlaceList
+                  offers={activeSort.func(activeOffers)}
+                  onCardTitleClick={onCardTitleClick}
+                  onPlaceCardMouseEnter={onPlaceCardMouseEnter}
+                />
+              </section>
+              <div className="cities__right-section">
+                <section className="cities__map map">
+                  <Map
+                    center={[latitude, longitude]}
+                    zoom={zoom}
+                    offers={activeOffers}
+                    hoveredOffer={hoveredOffer}
                   />
                 </section>
-                <div className="cities__right-section">
-                  <section className="cities__map map">
-                    <Map
-                      center={[latitude, longitude]}
-                      zoom={zoom}
-                      offers={activeOffers}
-                      hoveredOffer={hoveredOffer}
-                    />
-                  </section>
-                </div>
               </div>
-            )}
-          </div>
-        </main>
-      </div>
-    );
-  }
-}
+            </div>
+          )}
+        </div>
+      </main>
+    </div>
+  );
+};
 
 
 Main.propTypes = {
