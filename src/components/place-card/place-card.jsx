9567f42b27acdb14cb18faf9ad6f0d3211c33a28
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-08-07 05:06:44 +0200

    7.1. Fix PlaceCard image and favorite flag display

--- src/components/place-card/place-card.jsx
+++ src/components/place-card/place-card.jsx
@@ -12,6 +12,7 @@ const PlaceCard = (props) => {
   } = props;
 
   const {
+    isFavorite,
     isPremium,
     previewImage,
     price,
@@ -51,7 +52,7 @@ const PlaceCard = (props) => {
         place-card__image-wrapper`}
       >
         <a href="#">
-          <img className="place-card__image" src={`img/${previewImage}`} width="260" height="200" alt="Place image" />
+          <img className="place-card__image" src={`${previewImage}`} width="260" height="200" alt="Place image" />
         </a>
       </div>
       <div className="place-card__info">
@@ -62,11 +63,18 @@ const PlaceCard = (props) => {
             </b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button
+            className={`place-card__bookmark-button button
+              ${isFavorite && `place-card__bookmark-button--active`}`
+            }
+            type="button"
+          >
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
-            <span className="visually-hidden">To bookmarks</span>
+            <span className="visually-hidden">
+              {isFavorite ? `In bookmarks` : `To bookmarks`}
+            </span>
           </button>
         </div>
         <div className="place-card__rating rating">
